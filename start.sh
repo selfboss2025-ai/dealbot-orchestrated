d4c32e3d1333fe14f3d3146ad51631926242f057
selfboss2025-ai <selfboss2025@gmail.com>
2026-01-02 13:42:09 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,6 +2,10 @@
 
 # Avvia worker UK in background
 python workers/uk/worker_uk.py &
+WORKER_PID=$!
+
+# Aspetta che il worker sia pronto
+sleep 5
 
 # Avvia coordinatore in foreground
 python coordinator/main.py
